92b16cbbde9743fafb4b7fb145156029f31c7b9e
Fadel <fadel.g.heji@gmail.com>
2022-03-19 19:32:02 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(Distance){
+    return Math.abs(Distance-42)
+}
+
+function distanceFromHqInFeet(Distance){
+    return Math.abs((Distance-42)*264)
+}
+
+function distanceTravelledInFeet(Distance,Distance2){
+    return Math.abs((Distance-Distance2)*264)
+}
+
+function calculatesFarePrice(Distance,Distance2){
+    if (distanceTravelledInFeet(Distance,Distance2) <= 400){
+        return 0
+    }
+
+    else if (distanceTravelledInFeet(Distance,Distance2) <=2000) {
+        return (distanceTravelledInFeet(Distance,Distance2)-400)*0.02
+    }
+
+    else if (distanceTravelledInFeet(Distance,Distance2) <=2500){
+        return 25
+    }
+    
+    else {
+        return 'cannot travel that far'
+    }
+}
